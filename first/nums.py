277ca354e84f8738994c738492c7082eecf38e43
lenovo <grinkoys@yandex.ru>
2024-02-26 18:53:16 +0300

    init

--- first/nums.py
+++ first/nums.py
@@ -1,5 +1,10 @@
-def plus(a,b):
-    return(a+b)
+def plus(a, b):
+    return (a + b)
+
+
+def minus(a, b):
+    return (a - b)
+
 
 if __name__ == '__main__':
-        print(plus(2, 2))
+    print(minus(2, 2))
